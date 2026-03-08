ae453475ac210205090fe06bce6015b058a7952b
Cristian Beraha <cristian@cloudops.works>
2026-03-08 00:27:15 -0300

    chore: Extend S3 permissions to include `s3:GetObjectTagging` in policies

--- s3-roles.tf
+++ s3-roles.tf
@@ -48,6 +48,7 @@ locals {
                 "s3:GetEncryptionConfiguration",
                 "s3:GetLifecycleConfiguration",
                 "s3:GetObject",
+                "s3:GetObjectTagging",
                 "s3:GetObjectVersion",
                 "s3:GetReplicationConfiguration",
                 "s3:ListAllMyBuckets",
